d24d37d669aa1744ceafde1ab5c43a0079d790b5
HaydenHugh <hgbw69@163.com>
2026-06-26 15:12:29 +0800

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,9 +34,18 @@ android {
     }
 }
 
+
+// 新增：启用 core library desugaring
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+}
+
+
 kotlin {
     compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+        jvmTarget = JvmTarget.JVM_17
+        languageVersion = "1.8"
+        apiVersion = "1.8"
     }
 }
 
